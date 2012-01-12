1d54c757f053f8f44d18ee79e2952c348daed61a
Mathieu Hendey <mathieuahendey@gmail.com>
2012-01-12 19:53:53 +0000

    changed something small

--- Twertbot.py
+++ Twertbot.py
@@ -11,14 +11,15 @@ import urllib
 
 
 #fill in your own details here
-API = twitter.Api(consumer_key = '2QI0sN6XmHpWtyAOeDoPjA',
-                  consumer_secret = 'rU0xTVG2Q0Qn9ARCGxhcwQeZSNCyRRihwYPAnADlN0',
-                  access_token_key = '460552349-GpOwibMcie7xHVqUAzS7kXeww5qQIeO57YdTBV9Y',
-                  access_token_secret = 'gBhoMTZOLXjdMrxbyuaIOzMoDkGf3H66X2Vko0')
+API = twitter.Api(consumer_key='######################',
+                  consumer_secret='###################',
+                  access_token_key='##################',
+                  access_token_secret='###############')
 
 def prepareTweet(target):
     #This method composes the tweet
-    tweet = '$USERNAME FUS RO DAH!'        #enter your tweet after $USERNAME, don't delete $USERNAME, just leave one space after it
+    retries += 1
+    tweet = '$USERNAME '        #enter your tweet after $USERNAME, don't delete $USERNAME, just leave one space after it
     tweet = tweet.replace('$USERNAME', '@%s' % target.user.screen_name)
     if len(tweet) < 141:        #remember: tweets can only be 140 chars long
         return tweet
@@ -34,7 +35,7 @@ def makeTweet(target):
 def searcher():
     #searches for your query and returns a tweet
     print 'searching...'
-    results = API.GetSearch('Skyrim')        #replace query with your search term enclosed in 's
+    results = API.GetSearch(query)        #replace query with your search term enclosed in 's
     if results:
         for result in results:
                 return result
