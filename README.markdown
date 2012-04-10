872a2dd8a0c4ec76e953b0633c6084161cc425d9
Mathieu Hendey <mathieuahendey@gmail.com>
2012-04-10 16:20:19 +0100

    Added ebook functionality

---
+++ README.markdown
@@ -0,0 +1,12 @@
+A Twitter ebooks script
+=======================
+
+## Dependencies
+
+* [NLTK](http://nltk.github.com/install.html)
+* [python-twitter](http://code.google.com/p/python-twitter/)
+* [python-oauth2](https://github.com/simplegeo/python-oauth2)
+
+
+
+Go to [dev.twitter.com](http://dev.twitter.com) to generate your keys
--- Twertbot.py
+++ Twertbot.py
@@ -1,55 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import time
 import twitter
 from xml.dom import minidom
 import urllib
-#By Mathieu Hendey
-#http://www.mathieuhendey.com
+import nltk
+import random
+from nltk.corpus import brown
+from nltk.corpus import gutenberg
+from nltk.corpus import webtext
+from nltk.corpus import movie_reviews
 
-#This is a simple template for making a twitter bot. You need to make your own app at dev.twitter.com to generate
-#your consumer_key, consumer_secret, access_token_key and access_token_secret.
+def generateSentence():
+    corpus = random.randint(0,3)
+    if corpus == 0:
+        text = brown.words()
+    elif corpus == 1:
+        text = gutenberg.words()
+    elif corpus == 2:
+        text = webtext.words()
+    elif corpus == 3:
+        text = movie_reviews.words()
+    tweetString = ''
+    lengthOfTweet = random.randint(0,20)
+    len(text)
+    firstRun = True
+    blank = ' '
+    startOfWord = ''
+    startOfWordIndex = 0
+    startingWord = random.randint(0, (len(text) - 40))
+    punctuation = [".", ",", '"', ";", ":", "?", "!", ")", "(", "*", "[", "]", "‘", "“", "#"]
 
+    for x in xrange(startingWord,(startingWord + len(text))):
+        startOfWord = text[x]
+        if startOfWord ==".":
+                startOfWordIndex = x
+                break
+
+    for x in xrange(startOfWordIndex + 1, startOfWordIndex+lengthOfTweet):
+        if text[x] in punctuation:
+            tweetString = tweetString + text[x]
+
+        elif text[x] not in punctuation:
+            tweetString = tweetString + blank + text[x]
+    return tweetString
 
 
 #fill in your own details here
-API = twitter.Api(consumer_key='######################',
-                  consumer_secret='###################',
-                  access_token_key='##################',
-                  access_token_secret='###############')
+API = twitter.Api(consumer_key = '##################',
+                  consumer_secret = '##################',
+                  access_token_key = '##################-##################',
+                  access_token_secret = '##################')
 
-def prepareTweet(target):
+def prepareTweet():
     #This method composes the tweet
-    retries += 1
-    tweet = '$USERNAME '        #enter your tweet after $USERNAME, don't delete $USERNAME, just leave one space after it
-    tweet = tweet.replace('$USERNAME', '@%s' % target.user.screen_name)
+    tweet = generateSentence()
     if len(tweet) < 141:        #remember: tweets can only be 140 chars long
         return tweet
 
-def makeTweet(target):
+def makeTweet():
     #This method actually sends the tweet
-    print u'Tweeting @%s' %(target.user.screen_name)
-    tweet = prepareTweet(target)
-    API.PostUpdate(tweet, in_reply_to_status_id=target.id)
+    print 'Tweeting'
+    tweet = prepareTweet()
+    try:
+        API.PostUpdate(tweet)
+    except Exception, e:
+        time.sleep(10)
+        main()
     print 'Tweet successful!'
     return True
 
-def searcher():
-    #searches for your query and returns a tweet
-    print 'searching...'
-    results = API.GetSearch(query)        #replace query with your search term enclosed in 's
-    if results:
-        for result in results:
-                return result
+def main():
+    makeTweet()
 
 if __name__ == '__main__':
-    #main method, calls other methods and updates a counter to let you know how many tweets you've made
-    counter = 0
-    while True:
-            target = searcher()
-            if target:
-                result = makeTweet(target)
-                counter += 1
-                print '#%s Done' % counter
-                time.sleep(120)         #This is the time in seconds to wait between each tweet. If you are 
-                                        #searching for an obscure-ish term,
-                                        #set it to a high number, or you will tweet the same person twice, 
-                                        #Twitter will return a duplicate_status error and the script will end.
+    main()
+
